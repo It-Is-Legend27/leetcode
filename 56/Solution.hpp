4547889701cdbaebdfdcf278b325e9744a7f0cc6
Angel Badillo <badilloa022402@gmail.com>
2023-03-20 10:29:22 -0500

    added 56

---
+++ 56/Solution.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> output;
+
+        for(auto&& interval : intervals){
+            if(output.empty() || output.back()[1] < interval[0]){
+                output.push_back(interval);
+            }
+            else{
+                output.back()[1] = max(output.back()[1], interval[1]);
+            }
+        }
+        return output;
+    }
+};
---
+++ 56/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include "Solution.hpp"
+using namespace std;
+
+int main()
+{
+    vector<vector<int>> A = {{0,1}, {2,3}, {0,5}};
+    auto result = Solution().merge(A);
+}
