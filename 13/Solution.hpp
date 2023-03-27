21e8003be7af33e0922f97a1a29e844962e7ddf8
Angel Badillo <badilloa022402@gmail.com>
2023-03-27 10:42:01 -0500

    solved 13

--- 13/Solution.hpp
+++ 13/Solution.hpp
@@ -13,31 +13,61 @@
 #include <unordered_map>
 using namespace std;
 
-
-// TODO: Does not work for 6 instances of subtraction.
-/// I.e, IV, IX, XL, XC, CD, CM.
 class Solution
 {
 public:
-    static unordered_map<char,int> Numerals;
+    static unordered_map<char, int> Numerals;
+
+    static unordered_map<string, int> Subs;
 
     int romanToInt(string s)
     {
         int val = 0;
 
+        // For each "subtraction", find and remove each instance,
+        // and update val
+        for (auto &&i : Subs)
+        {
+            int idx = 0;
+
+            // Search for current subtraction,
+            // and remove all instances from string, while
+            // updating val
+            do
+            {
+                idx = s.find(i.first);
+                if(idx != string::npos)
+                {
+                    s.erase(idx, i.first.size());
+                    val += i.second;
+                }
+            } while (idx != string::npos);
+        }
+        
+        // Add all remaining symbols as normal
         for (auto &&c : s)
             val += Numerals[c];
+
         return val;
     }
 };
 
-unordered_map<char,int> Solution::Numerals =
-{
-    {'I', 1},
-    {'V', 5},
-    {'X', 10},
-    {'L', 50},
-    {'C', 100},
-    {'D', 500},
-    {'M', 1000}
-};
+// 
+unordered_map<char, int> Solution::Numerals =
+    {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}};
+
+unordered_map<string, int> Solution::Subs =
+    {
+        {"IV", 4},
+        {"IX", 9},
+        {"XL", 40},
+        {"XC", 90},
+        {"CD", 400},
+        {"CM", 900}};
