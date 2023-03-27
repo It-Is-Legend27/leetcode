8f3661802d654236712cd34d464d9223475556ee
Angel Badillo <badilloa022402@gmail.com>
2023-03-27 10:54:24 -0500

    solved 240

---
+++ 240/main.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file main.cpp
+ * @author Angel Badillo Hernandez (https://github.com/It-Is-Legend27)
+ * @brief Solution to LeetCode Problem 240.
+ * @version 0.1
+ * @date 2023-03-27
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include <iostream>
+#include <vector>
+#include "Solution.hpp"
+using namespace std;
+
+int main()
+{
+    Solution s;
+    
+}
